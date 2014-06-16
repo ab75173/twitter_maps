ae7001b811769aae9afa2ecde99262b86bbee351
OKitsMikael <mgd4@njit.edu>
2014-06-16 14:14:59 -0400

    initial twitter omniauth steps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616155412) do
+ActiveRecord::Schema.define(version: 20140616172256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20140616155412) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "zip_code"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
