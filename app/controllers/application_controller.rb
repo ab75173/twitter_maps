85f925c1447ee398a2a2a353f2e37a748ea72ae0
rmlatz <rmlatz@gmail.com>
2014-06-16 11:34:26 -0400

    Better navigation, allows zipcode updating

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :zip_code
+    devise_parameter_sanitizer.for(:account_update) << :zip_code
+
   end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
