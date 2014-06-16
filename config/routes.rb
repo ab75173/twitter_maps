ef85ba6bbc3fb9b5b01354590252769c8ce0322f
maryhipp <maryhipp@gmail.com>
2014-06-16 09:40:23 -0400

    fixed typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   resources :events, only: [:index, :show]
-  resources :users, only [:show] do
-    resources :events, only [:index, :show]
+  resources :users, only: [:show] do
+    resources :events, only: [:index, :show]
   end
 
 
