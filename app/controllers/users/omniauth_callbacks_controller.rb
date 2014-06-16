651def2ca04fd917e534defd2d461021aa5321d7
OKitsMikael <mgd4@njit.edu>
2014-06-16 16:37:27 -0400

    twitter authentication working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
     else
       session["devise.twitter_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to root_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users, controllers: [omniauth_callbacks: 'users/omniauth_callbacks']
+  devise_for :users, except: [:show], :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'welcome#index'
   resources :events, only: [:index, :show]
   resources :users, only: [:show] do
     resources :events, only: [:index, :show]
   end
 
-  devise_scope :user do
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  # devise_scope :user do
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 
 
 end
