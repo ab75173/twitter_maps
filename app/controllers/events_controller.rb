83e2939a73e6d61cde01a6a19b44e64baee9a0f8
GS888 <gsnydermail@gmail.com>
2014-06-16 14:08:53 -0400

    added cookie locations to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,26 @@
 class EventsController < ApplicationController
 
   def index
+
+    # cookie maker:
+    cookies.delete(:lat)
+    cookies.delete(:long)
+    cookies[:lat] = 34.052234
+    cookies[:long] = -118.243685
+
     events = Event.all
     nearby_events = []
 
-    @location_json = HTTParty.get('http://freegeoip.net/json/')
-    user_lat = @location_json["latitude"]
-    user_long = @location_json["longitude"]
+    # Getting user lat and long through IP address:
+    # @location_json = HTTParty.get('http://freegeoip.net/json/')
+    # user_lat = @location_json["latitude"]
+    # user_long = @location_json["longitude"]
+
+    # Getting user lat and long from the cookie created above! TEST!!!
+    user_lat = cookies["lat"]
+    user_long = cookies["long"]
+
+    # Defining users local area (about 20 mile radius)
     user_lat_max = user_lat + 0.20
     user_lat_min = user_lat - 0.20
     user_long_max = user_long + 0.20
