c50ea420bea7459694d04b044bdc65d47a0c8471
maryhipp <maryhipp@gmail.com>
2014-06-16 14:10:44 -0400

    Events controller tinkering & zip code field for non logged in user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,18 +9,18 @@ class EventsController < ApplicationController
     cookies[:long] = -118.243685
 
     events = Event.all
-    nearby_events = []
 
-    # Getting user lat and long through IP address:
-    # @location_json = HTTParty.get('http://freegeoip.net/json/')
-    # user_lat = @location_json["latitude"]
-    # user_long = @location_json["longitude"]
+    @nearby_events = []
+    if user_signed_in?
+      @user = current_user
+      user_lat = @user.latitude
+      user_long = @user.longitude
+    else
 
-    # Getting user lat and long from the cookie created above! TEST!!!
-    user_lat = cookies["lat"]
-    user_long = cookies["long"]
+      user_lat = cookies["lat"]
+      user_long = cookies["long"]
+    end
 
-    # Defining users local area (about 20 mile radius)
     user_lat_max = user_lat + 0.20
     user_lat_min = user_lat - 0.20
     user_long_max = user_long + 0.20
@@ -31,11 +31,9 @@ class EventsController < ApplicationController
       event_long = event.longitude
 
       if event_long < user_long_max && event_long > user_long_min && event_lat < user_lat_max && event_lat > user_lat_min
-     nearby_events.push(event)
+        @nearby_events.push(event)
       end
     end
-
-     @nearby_events = nearby_events
   end
 
   def show
