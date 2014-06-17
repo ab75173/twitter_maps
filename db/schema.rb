d3da79092db725b97d50c228c046a690c535cee5
GS888 <gsnydermail@gmail.com>
2014-06-17 12:35:14 -0400

    updating

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140617140828) do
+ActiveRecord::Schema.define(version: 20140617151427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
