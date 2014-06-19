1ac4181f4ab4853f069347f5e2ced124d3eb2064
maryhipp <maryhipp@gmail.com>
2014-06-19 11:09:35 -0400

    reconfiguring js files

---
+++ app/assets/javascripts/every_page/signup.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.registration').on('click', function(){
+    $('.registration').toggleClass('visuallyhidden');
+    $('.showOptions').toggleClass('visuallyhidden');
+  });
+
+});
+
+
+
