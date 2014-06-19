2c4fe03424bef782f587357c2de2caab0f289bbd
maryhipp <maryhipp@gmail.com>
2014-06-19 10:48:57 -0400

    deleted redundant google map js file

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 20140618213750) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "authentications", force: true do |t|
-    t.integer "user_id"
-    t.string  "provider"
-    t.string  "uid"
-  end
-
   create_table "events", force: true do |t|
     t.string   "hashtag"
     t.string   "title"
