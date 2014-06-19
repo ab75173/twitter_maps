09fb1be1f46333468408112979229f5c2e575742
maryhipp <maryhipp@gmail.com>
2014-06-19 11:17:43 -0400

    js

--- app/assets/javascripts/signup.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.registration').on('click', function(){
-    $('.registration').toggleClass('visuallyhidden');
-    $('.showOptions').toggleClass('visuallyhidden');
-  });
-});
-
-
-
